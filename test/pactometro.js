5c8994eb67fce8e5822d883773990ffcdae988e3
David Vaquero <pepesan@gmail.com>
2023-03-07 19:55:40 +0100

    nuevo test de oposicion y coalicion

--- test/pactometro.js
+++ test/pactometro.js
@@ -5,7 +5,7 @@ fixture('Getting Started')
     // given
     .page('https://cursosdedesarrollo.com/pactometro/');
 
-test('ClickAndDisabled', async t => {
+test('TestCoalicion Psoe-Podemos', async t => {
     // When
     await t.click("#part-PSOE");
     // Then
@@ -17,6 +17,32 @@ test('ClickAndDisabled', async t => {
     await t.expect(Selector("#num-sies").innerText)
         .eql("120");
     // Then
-    await t.expect(Selector("#part-PSOE").getAttribute())
+    await t.expect(Selector("#part-PSOE").getAttribute("style"))
     .eql("display: none;");
+    // When
+    await t.click("#reset");
+    // Then
+    await t.expect(Selector("#num-sies").innerText)
+        .eql("0");
+});
+
+test('Coalicion + Oposicion', async t => {
+    // When
+    await t.click("#part-PSOE");
+    // When
+    await t.click("#part-Podemos");
+    // When
+    await t.click("#sies");
+    // Then
+    await t.expect(Selector("#num-sies").innerText)
+        .eql("155");
+    // When
+    await t.click("#part-PP");
+    // When
+    await t.click("#part-Vox");
+    // When
+    await t.click("#noes");
+    // Then
+    await t.expect(Selector("#num-noes").innerText)
+        .eql("141");
 });
